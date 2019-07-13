bd0a7fedb27a3c4b6664256c4811f99d85aeee92
saurabh thareja <saurabhthareja90@gmail.com>
2019-07-14 09:20:22 +1000

    updated ansible module to enable ntp service on boot

--- installApp.sh
+++ installApp.sh
@@ -1,11 +1,7 @@
 
 #clone challenge app
-git clone https://github.com/AfterpayTouch/recruitment-challenge-1.git flaskapp
+git clone https://github.com/AfterpayTouch/recruitment-challenge-1.git /flaskapp
 #cd to flaskapp
-cd flaskapp
+cd /flaskapp
 #install requirements
 pip3 install -r requirements.txt
-#go to the app
-cd flaskapp/
-#run the app
-gunicorn3 tiny_app:app &
